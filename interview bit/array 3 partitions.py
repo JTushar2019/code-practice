b047df64b30a7861d6bf9d231218a083952418df
tushar <tushar.singh1999.com>
2021-07-29 10:10:31 +0530

    documentation

--- interview bit/array 3 partitions.py
+++ interview bit/array 3 partitions.py
@@ -10,8 +10,8 @@ class Solution:
         from itertools import accumulate as acc
         d = defaultdict(int)
         d[arr[0]] = 1
-        rightsum = list(acc(arr[::-1], operator.add))[::-1]
-        leftsum = list(acc(arr, operator.add))
+        rightsum = list(acc(arr[::-1], operator.add))[::-1]  # left accumulating sum
+        leftsum = list(acc(arr, operator.add))  # right accumulating sum
         result = 0
         for j in range(1, l - 1):
             temp = leftsum[j]
@@ -19,9 +19,17 @@ class Solution:
                 d[leftsum[j]] += 1
                 continue
             middle = temp / 2
-            if middle == rightsum[j + 1]:
+            # if sum till now is even.. it can be broken in 2 parts. left and middle partition
+            # so breaking it into two parts. and check if the one part sum seen till now in hash
+            # in left of j or
+            # in my coding, default dict. it will give zero if key not present and add it in too.
+
+            if middle == rightsum[j + 1]:  # if the broken portion is sum equal to right partition.
                 result += d[middle]
+                # doing this cause the broken half sum could be formed at different length
+
             d[leftsum[j]] += 1
+            # also for current sum, add it to last seen value or if seen first time it will just do 0 + 1
 
         print(result)
         return result

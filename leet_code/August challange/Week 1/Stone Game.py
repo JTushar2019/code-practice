58de5c055b1466554102ea11dd66884a6ccbbb3c
JTushar2019 <tushar.singh1999@gmail.com>
2021-08-05 20:13:03 +0530

    another day

--- leet_code/August challange/Week 1/Stone Game.py
+++ leet_code/August challange/Week 1/Stone Game.py
@@ -1,35 +1,41 @@
 # https://leetcode.com/explore/featured/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/
+import operator
 
 
 class Solution:
     def stoneGame(self, piles) -> bool:
-        arr = [0, 0]
-        flag = False
-        s = sum(piles)
-        t = (s // 2)
+        # arr = [0, 0]
+        # flag = False
+        #
+        # def utility(chance, i, j):
+        #     nonlocal flag, piles, arr
+        #     if i > j:
+        #         if arr[0] > arr[1]:
+        #             flag = True
+        #         return
+        #
+        #     arr[chance] += piles[i]
+        #
+        #     utility((chance + 1) % 2, i + 1, j)
+        #
+        #     if flag:
+        #         return
+        #
+        #     arr[chance] -= piles[i]
+        #     arr[chance] += piles[j]
+        #
+        #     utility((chance + 1) % 2, i, j - 1)
+        #
+        #     arr[chance] -= piles[j]
+        #
+        # utility(0, 0, len(piles) - 1)
+        # return flag
 
-        def utility(chance, i, j):
-            nonlocal flag, piles, arr, t
-            if arr[1] > t:
-                return
-            if i > j:
-                if arr[0] > arr[1]:
-                    flag = True
-                return
+        return True
 
-            arr[chance] += piles[i]
-
-            utility((chance + 1) % 2, i + 1, j)
-
-            if flag:
-                return
-
-            arr[chance] -= piles[i]
-            arr[chance] += piles[j]
-
-            utility((chance + 1) % 2, i, j - 1)
-
-            arr[chance] -= piles[i]
-
-        utility(0, 0, len(piles) - 1)
-        return flag
+# as logic says, anybody can get any pile of stone. cause there is not any greedy approch to win.
+# so one has to just play by luck. and by doing that, one can see that any perticular pile can be given
+# to either player by a more than one sequnce of distibution.
+# so as total piles is odd
+# so equal partition cant be done.. hence one is bound to have greater share than the other
+# so winning is always possible.

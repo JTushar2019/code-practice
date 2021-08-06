a4779bc124d001cd49862670d1a4642301f1e8e0
JTushar2019 <tushar.singh1999@gmail.com>
2021-08-06 13:02:32 +0530

    system git testing

---
+++ leet_code/August challange/Week 1/N-ary Tree Level Order Traversal.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/explore/featured/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3871/
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def levelOrder(self, root):
+        from collections import defaultdict, deque
+        if not root:
+            return []
+        level = defaultdict(lambda: 0)
+        result = defaultdict(list)
+        q = deque()
+        q.append(root)
+        level[root] = 0
+        visited = set()
+        visited.add(root)
+        while q:
+            node = q.popleft()
+            result[level[node]].append(node.val)
+
+            for each in node.children:
+                level[each] = level[node] + 1
+                if each not in visited:
+                    q.append(each)
+                    visited.add(each)
+
+        print(list(result.values()))
+        return list(result.values())
